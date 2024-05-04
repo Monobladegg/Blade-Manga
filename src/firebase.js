d0048512768a9fb001f141c4f47d06a8a30d3a2e
Monoblade <157546326+Monobladegg@users.noreply.github.com>
2024-05-05 00:32:18 +0300

    Шифровка

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB3GMQrmN8iGqb5Ij0tqV60WZ3AlPZeHZM",
-  authDomain: "shopmanga-d39b3.firebaseapp.com",
-  projectId: "shopmanga-d39b3",
-  storageBucket: "shopmanga-d39b3.appspot.com",
-  messagingSenderId: "652854161068",
-  appId: "1:652854161068:web:32fdcbc8e4187fa3efb909"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
